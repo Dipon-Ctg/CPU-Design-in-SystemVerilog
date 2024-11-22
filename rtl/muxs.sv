8d2fb9a0ca81c6019e7bb76743229b6094ee0e3a
Dipon Barua <85799541+Dipon-Ctg@users.noreply.github.com>
2024-11-22 14:09:04 +0100

    Create muxs.sv

---
+++ rtl/muxs.sv
@@ -0,0 +1,29 @@
+//Multiplexers
+
+//Mux1
+module mux1 (
+  input logic sel1,
+  input logic [11:0] a,b,
+  output logic [11:0] mux1_out
+);
+  assign mux1_out = (sel1==1'b0) ? a:b;
+endmodule
+  
+//Mux2
+module mux2 (
+  input logic sel2,
+  input logic [11:0] a,b,
+  output logic [11:0] mux2_out
+);
+  assign mux2_out = (sel2==1'b0) ? a:b;
+endmodule
+
+//Mux3
+module mux3 (
+  input logic sel3,
+  input logic [11:0] a,b,
+  output logic [11:0] mux3_out
+);
+  assign mux3_out = (sel3==1'b0) ? b:a;
+endmodule
+
