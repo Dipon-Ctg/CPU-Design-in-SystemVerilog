e857aaf036ad3b4ec821dfacf4562978b6a996bf
amrshahat <69316417+amrshahat@users.noreply.github.com>
2024-11-25 13:28:15 -0500

    Update ins_mem.sv
    adjust the address width , 
    
    allow memory to be writable via write enable , the plan is to use testbench to write 16 entries at the beginning of the run

--- rtl/ins_mem.sv
+++ rtl/ins_mem.sv
@@ -1,27 +1,33 @@
 //Instruction Memory
 module instruction_memory (
   input logic clk,reset,
-  input logic [11:0] read_address,
+  input logic wr_en,
+  input logic [3:0] addr,
+  input logic [11:0] write_data,
   output logic [11:0] instruction
 );
   
-  logic [11:0] memory_array [23:0];	// Memory array to hold instructions
+  logic [11:0] memory_array [15:0];	// Memory array to hold instructions
   
   always_ff @(posedge clk or posedge reset)
     begin
       if (reset)
         begin
-        for (int k =0; k <24; k++)
+          for (int k =0; k <16; k++)
           begin
             memory_array[k] <= 12'b0;
           end
           instruction <= 12'b0;		//Clean output during reset
         end
+      else if (write_en)
+        begin
+          mem[addr] <= write_data; 
+        end
       else
         //Fetch the instruction if the address is valid
         begin
-          if (read_address < 24)
-            instruction <= memory_array [read_address];
+          if (addr < 16)
+            instruction <= memory_array [addr];
           else 
             instruction <= 12'b0; //Handle invslid address
         end
