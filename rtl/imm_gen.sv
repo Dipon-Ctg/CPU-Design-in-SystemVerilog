c3e8f376395109a04b3ae5309f20a57aa98f932a
Dipon Barua <85799541+Dipon-Ctg@users.noreply.github.com>
2024-11-30 02:25:25 +0100

    Update imm_gen.sv

--- rtl/imm_gen.sv
+++ rtl/imm_gen.sv
@@ -1,10 +1,19 @@
 // Immediate Generator
-
+/*
 module ImmGen (
   input logic [11:0] instruction,
   output logic [11:0] ImmExit
 );
   // Not added all features
+  assign ImmExit = instruction;
+  
+endmodule
+*/
+module ImmGen (
+  input logic [1:0] instruction,
+  output logic [1:0] ImmExit
+);
+  
   assign ImmExit = instruction;
   
 endmodule
