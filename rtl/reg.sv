894c00313c0f4392eca07fb74fb6fe1882625cb1
Dipon Barua <85799541+Dipon-Ctg@users.noreply.github.com>
2024-11-30 02:28:06 +0100

    Update reg.sv

--- rtl/reg.sv
+++ rtl/reg.sv
@@ -1,5 +1,5 @@
 //Register File
-
+/*
 module register_file (
   input logic clk, reset, RegWrite,
   input logic [1:0] rs1,rs2,rd,
@@ -20,7 +20,7 @@ module register_file (
           Registers[2] <=12'b0;
           Registers[3] <=12'b0;
         end
-        */
+        
         begin
           for (int k=0; k<4; k=k+1)
             begin
@@ -37,4 +37,35 @@ module register_file (
   assign rd2 = Registers[rs2];
   
 endmodule
+*/
+
+module register_file (
+  input logic clk, reset, RegWrite,
+  input logic [1:0] rs1,rs2,rd,
+  input logic [3:0] WriteData,
+  output logic [3:0] rd1,rd2
+);
+  
+  logic [3:0] Registers [3:0];
+  
+  always_ff @(posedge clk or posedge reset)
+    begin
+      if (reset)
+        begin
+          for (int k=0; k<4; k=k+1)
+            begin
+              Registers[k] <= 4'b0;
+            end
+        end
+      else if (RegWrite)
+        begin
+          Registers[rd] <= WriteData;
+        end
+    end
+  
+  assign rd1 = Registers[rs1];
+  assign rd2 = Registers[rs2];
+  
+endmodule
+
 
