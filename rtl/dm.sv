522e2674c475377b87853d1918ea5617e9c8a798
Dipon Barua <85799541+Dipon-Ctg@users.noreply.github.com>
2024-11-30 02:24:42 +0100

    Update dm.sv

--- rtl/dm.sv
+++ rtl/dm.sv
@@ -1,5 +1,5 @@
 //Data Memory
-
+/*
 module data_memory (
   input logic clk,reset,MemWrite,MemRead,
   input logic [11:0] address,wd2,
@@ -24,4 +24,30 @@ module data_memory (
   assign MemData_out = (MemRead) ? D_Memory [address]: 12'b00;
   
 endmodule
+*/
+module data_memory (
+  input logic clk,reset,MemWrite,MemRead,
+  input logic [3:0] address,wd2,
+  output logic [3:0] MemData_out
+);
+  logic [3:0] D_Memory [15:0];
+  
+  always_ff @(posedge clk or posedge reset)
+    begin
+      if (reset)
+        begin
+          for (int k=0; k<16; k++)
+            begin
+              D_Memory [k] <= 4'b00;
+            end
+        end
+      else if (MemWrite)
+        begin
+          D_Memory [address] <= wd2;
+        end
+    end
+  assign MemData_out = (MemRead) ? D_Memory [address]: 4'b00;
+  
+endmodule
+
 
